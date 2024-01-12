d42910af65ef5484f470f69e109f9ea5ac6aa3b1
dantincu <tincuvd@gmail.com>
2024-01-12 17:29:24 +0200

    Updated the Notes app launcher page

---
+++ turmerik-notes/launch/index.e3989a83.js
@@ -0,0 +1,2 @@
+let t=!1;const e=()=>{if(!t)for(let e of(t=!0,document.querySelectorAll("a[data-url]"))){let t=e.getAttribute("data-url"),r=e.getAttribute("href");t&&!r&&e.setAttribute("href",t)}};document.addEventListener("DOMContentLoaded",e),("complete"===document.readyState||"interactive"===document.readyState)&&setTimeout(e,1);
+//# sourceMappingURL=index.e3989a83.js.map
